0119cce77592e3ec613b83799b371e5e917cab58
Kody Buss <kody.buss@gmail.com>
2017-01-31 01:22:46 -0600

    CAPSTONE BEGIN
    Added more unique infinite backgrounds to the game in order to transition it toward my capstone project idea. I used a repeating material that has its texture programmatically offset over time in order to produce this effect.

---
+++ Assets/Scripts/movingBackground.cs
@@ -0,0 +1,29 @@
+﻿//This script creates an infinitely scrolling background
+//on a given quad
+
+//Solution created from a mix of many authors
+
+using UnityEngine;
+using System.Collections;
+
+public class movingBackground: MonoBehaviour 
+{
+	private float currentOffset = 0;
+	public float speed = .1f;
+
+	void Start() {
+		currentOffset += Time.deltaTime * speed; 
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		currentOffset += Time.deltaTime * speed;
+		if (currentOffset > 1.0f)
+		{
+			currentOffset -= 1.0f;
+		}   
+
+		GetComponent<Renderer>().material.mainTextureOffset = new Vector2(currentOffset, 0); 
+	}
+}
