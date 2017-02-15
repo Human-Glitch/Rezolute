817eee89b79d9ddc08e82e5b87f1b185a2083be6
Kody Buss <kody.buss@gmail.com>
2017-02-15 00:15:41 -0600

    Fleshing out the design with scfi assets. Created scripts that makes the infinite background follow the player. Added scripts for parallax effect for clouds/smoke?

---
+++ Assets/Scripts/takeCoin.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class takeCoin : MonoBehaviour {
+	public Text coinCounter;
+	public GameObject gameOverScreen;
+	public GameObject allCoins;
+
+	private AudioSource coinChing;
+
+	private int score = 0;
+	private int totalCoins = 17;
+
+	// Use this for initialization
+	void Start () {
+		coinChing = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (score == totalCoins ) 
+		{
+			allCoins.SetActive (true);
+			gameOverScreen.SetActive (true);
+
+			Time.timeScale = 0;
+		}
+
+	}
+
+	public void IncrementCounter(){
+		coinCounter.text = string.Format("Coin: {0}", ++score);
+		coinChing.Play ();
+	}
+		
+}
