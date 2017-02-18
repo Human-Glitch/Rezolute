67cbf3f6a1bf1aefac682f2c3c53e9337ee3dbf6
Kody Buss <kody.buss@gmail.com>
2017-02-17 23:09:17 -0600

    Major Progress on moving platforms and objects
    4 new scripts specifcally for moving obstacles
    Translation and Rotation Scripts included

---
+++ Assets/Scripts/Action Scripts/ClampedRotation.cs
@@ -0,0 +1,35 @@
+﻿//http://gamedev.stackexchange.com/questions/130635/how-to-rotate-within-a-fixed-interval-in-unity
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClampedRotation : MonoBehaviour {
+	float delay = 3f;
+
+	// Specify how many degrees you want to rotate from the initial orientation.
+	public Vector3 amplitude = new Vector3(30, 0, 0);
+
+	// Specify how many seconds a complete loop should take.
+	public float period = 6f;
+
+	// Cache initial orientation so we can rotate relative to that pose.
+	Quaternion _initialOrientation;
+
+	// Perform caching on Start.
+	void Start() {
+		_initialOrientation = transform.localRotation;
+	}
+
+	void Update ()
+	{
+		// Calculate how far along we should be in the wave.
+		float phase = Mathf.PI * 2f * Time.time / period;
+
+		// Calculate the angle at this part of the wave.
+		Vector3 angle = amplitude * Mathf.Cos (phase);
+
+		// Rotate by angle relative to our initial orientation.
+		transform.localRotation = _initialOrientation * Quaternion.Euler (angle);
+	}
+}
---
+++ Assets/Scripts/Action Scripts/IntervalRotate.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntervalRotate : MonoBehaviour 
+{
+	private bool hasStopped;
+	private float rotation = 0; 
+	private float rotationAmt = 0;
+
+	public float targetRotation = 90f;
+	public float rotationDelay = 0f;
+	public float rotateSpeed = 1f;
+
+	void Start()
+	{
+		hasStopped = false;
+	}
+
+	void Update ()
+	{
+		rotationAmt = rotateSpeed * Time.deltaTime;
+
+		if (Mathf.Abs(rotation) < targetRotation) 
+		{
+			Debug.Log ("Entered Normal");
+			transform.Rotate (0, 0, rotationAmt);
+			rotation += rotationAmt;
+		} 
+		else
+		{
+			Debug.Log ("Else reached");
+
+			if(hasStopped == false)
+			{
+				targetRotation = 90.0f;
+				delayRotation ();
+			}
+		}
+	}
+
+	public void delayRotation()
+	{
+		StartCoroutine ("delayRotationCo");
+	}
+
+	public IEnumerator delayRotationCo(){
+
+		hasStopped = true;
+		rotation = 0;
+		rotationAmt = 0;
+
+		yield return new WaitForSeconds (rotationDelay);
+		Debug.Log ("Block Stopped");
+
+		hasStopped = false;
+	}
+
+}
