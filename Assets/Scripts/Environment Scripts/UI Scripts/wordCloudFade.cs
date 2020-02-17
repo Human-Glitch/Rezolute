7d82437039733edb07f96f11345ba36581e8b58f
Kody Buss <kody.buss@gmail.com>
2020-02-16 18:13:44 -0600

    Fixed file name

--- Assets/Scripts/Environment Scripts/UI Scripts/wordCloudFade.cs
+++ Assets/Scripts/Environment Scripts/UI Scripts/wordCloudFade.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class wordCloudFade : MonoBehaviour 
+public class WordCloudFade : MonoBehaviour 
 {
     #region Members
 
